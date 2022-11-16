46ce090803015c6064725fcf8d5ed4a4902f7d8b
wedia <wediaishaq17@gmail.com>
2022-11-16 19:04:09 +0000

    express

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click',makeReq)
+
+async function makeReq(){
+    const rap = document.querySelector('input').value
+
+    try{
+      const response = await fetch(`https://raps-api.herokuapp.com/api/${rap}`)
+      const data = await response.json()
+      console.log(data)
+      document.querySelector('h2').textContent = data.name
+    }catch(error){
+        console.log(error)
+    }
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+
+app.use(cors())
+//app.use(express.static('public'))
+
+let rappers = {
+    '21 savage':{
+        name:'Sheyaa Bin Abraham Joseph',
+        age:25,
+        birthLocation:'London,England'
+    },
+    'chance the rapper':{
+        name:'Chancellor Bennet',
+        age:28,
+        birthLocation:'Illonois,USA'
+    },
+    'dylan':{
+        name:'Dylan',
+        age: 'Dylan',
+        birthLocation:'Dylan'
+    }
+}
+
+app.get('/',(request,respond)=>{
+    respond.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name',(request,response)=>{
+    const rapperInfor = request.params.name.toLowerCase()
+    if(rappers[rapperInfor]){
+        response.json(rappers[rapperInfor])
+    }else{
+        response.json(rappers['dylan'])
+    }
+})
+
+app.listen(PORT,()=>{
+    console.log(`The server is running on PORT ${PORT}`)
+})
