9ede656202c789e52840d50ba47864a327b2ebf5
wedia <wediaishaq17@gmail.com>
2022-12-15 15:14:03 +0000

    CORS changes

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const cors = require('cors')
 const PORT = 8000
 
 
-app.use(cors({
-    origin: 'https://foo.com'
-}));
+app.use((req,res,next) =>{
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With,Content-Type,Accept'
+    )
+    next()
+});
 app.use(express.static('public'));
 
 let rappers = {
