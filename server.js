90d738af160da71dca947ee1ca72d9063a844784
wedia <wediaishaq17@gmail.com>
2022-12-15 16:46:44 +0000

    CORS changes

--- server.js
+++ server.js
@@ -4,15 +4,10 @@ const cors = require('cors')
 const PORT = 8000
 
 
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+app.use(cors({
+    origin:'https://rappers.onrender.com/api'
+}))
+
 app.use(express.static('public'));
 
 let rappers = {
